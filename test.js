98bb260c55d3f1c417e482b0baee2ea1a73e7813
andre-QMC <75270214+andre-QMC@users.noreply.github.com>
2022-06-29 15:04:30 -0300

    initial commit

---
+++ test.js
@@ -0,0 +1,35 @@
+class Dialog {
+    element
+
+    /**
+     * Builds the Dialog instance binding and HTML element.
+     * 
+     * @param {HTMLElement} element https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+     */
+    constructor(element) {
+
+    }
+
+    /**
+     * Moves the dialog when grabbed by user.
+     * 
+     * @param {MouseEvent} ev https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+     */
+    move(ev) {
+
+    }
+
+    /**
+     * Opens the dialog, showing it on the default initial position.
+     */
+    open(){
+
+    }
+
+    /**
+     * Closes the dialog, removing it from the screen.
+     */
+    close(){
+
+    }
+}
